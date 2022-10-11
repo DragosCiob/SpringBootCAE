7812391eff0d9b6a1b0cdac4569dd5f9e280de73
DragosCiob <ciobanudragos80@gmail.com>
2022-10-11 16:25:43 +0300

    Initial Commit - configuration

---
+++ src/main/java/ro/siit/SpringBootCAE/SpringBootCaeApplication.java
@@ -0,0 +1,13 @@
+package ro.siit.SpringBootCAE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCaeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootCaeApplication.class, args);
+	}
+
+}
