3f9c315ba74fb68b593403bd173246aa58a21166
Dragos <ciobanudragos80@gmail.com>
2022-11-11 15:31:57 +0200

    schemaSQL, dataSQL

---
+++ src/main/resources/data.sql
@@ -0,0 +1,35 @@
+    INSERT INTO users(
+	user_id, password, team, first_name, second_name, username)
+	VALUES ('8410c252-8ec3-44e4-9c93-35775f32c687', '$2y$10$MsMuWIM8urx7WEO2FBqude3jev2KTFOMf6vy9Og4qXHrBRI7YlVTq', 'CAELEAD', 'Magdalena', 'Ricard', 'magdalenaR');
+	INSERT INTO users(
+    user_id, password, team, first_name, second_name, username)
+    VALUES ('d0742d47-0609-4774-ace3-9da696f698df', '$2y$10$IiJvr2kYeOiFvfARz/Woc.uZ/5lC5bT5WvdrJqTKQBEKigrS/tLYm', 'CAELEAD', 'Thomas', 'Gall', 'thomasR');
+
+
+    	INSERT INTO users(
+        user_id, password, team, first_name, second_name, username)
+        VALUES ('e0954b61-88a3-4a16-8d6e-a36becc45f18', '$2y$10$nSUbPMY6w166ZUAGvNupZ.rbZf10Qy1hrrARc6pgeqWQyqM837JcC', 'CFD', 'Dragos', 'Ciobanu', 'ciobanuD');
+        INSERT INTO users(
+        user_id, password, team, first_name, second_name, username)
+        VALUES ('91920fe5-6443-4172-8e31-c325b1c93fab', '$2y$10$C3T2zdfEHdazL6e96qZ8VO1H6/oKsqyuU02rjhlkIQ6qfV0uuhPJ2', 'CFD', 'Eder', 'Mann', 'ederM');
+
+        INSERT INTO users(
+        user_id, password, team, first_name, second_name, username)
+        VALUES ('38909909-1713-40dc-977b-a46f291ed303', '$2y$10$RdRf3m/ZL6w4a3m/bt5gC.wN25V6OZ6bjdQiwOPjmbJDyKhRNsmzi', 'VALIDATION', 'Hans', 'Jirowez', 'hansJ');
+        INSERT INTO users(
+        user_id, password, team, first_name, second_name, username)
+        VALUES ('e1acc4ef-5e4d-4f50-b064-79ca8fdabae7', '$2y$10$YFZKaqjtTzDxh/z3v9WOKe8zgHRyS7CJDphbOq2mSFmn7ngP/gBc.', 'VALIDATION', 'Ana', 'Sandu', 'anaS');
+
+        INSERT INTO users(
+        user_id, password, team, first_name, second_name, username)
+        VALUES ('0ed7f953-6bc3-4d46-b90a-02a6519ede4a', '$2y$10$RdRf3m/ZL6w4a3m/bt5gC.wN25V6OZ6bjdQiwOPjmbJDyKhRNsmzi', 'MECHANICAL', 'Bjorn', 'Damsgard', 'bjornD');
+        INSERT INTO users(
+        user_id, password, team, first_name, second_name, username)
+        VALUES ('358055ce-be94-4aad-85b8-73722b0c3a4d', '$2y$10$asr1OMbqZ9UL2JwpC8Sk/eOtqIgxRc6Si43zyjdGGQQzXShP.cPh2', 'MECHANICAL', 'Sara', 'Arrancci', 'saraA');
+
+        INSERT INTO users(
+        user_id, password, team, first_name, second_name, username)
+        VALUES ('70edb7cf-4138-4eb9-956c-8df0221e7de0', '$2y$10$kimCXuvMePcSBugZbAep7OOfTuf7trfGlLWY8F8boalYrC38Z.TK.', 'NVH', 'Marcello', 'Solara', 'marcelloS');
+        INSERT INTO users(
+        user_id, password, team, first_name, second_name, username)
+        VALUES ('4312049a-e5d4-4c6d-958e-c5f8b74397d4', '$2y$10$hiYcmhmBNAG72oi7VXwnVejaN/X9ggGd5RXNHy99NA./PIVeVu6o.', 'NVH', 'Natalie', 'Ruth', 'natalieR');
---
+++ src/main/resources/schema.sql
@@ -0,0 +1,41 @@
+
+        create table users (
+           user_id uuid not null,
+            password varchar(64) not null,
+            team varchar(255),
+            first_name varchar(32) not null,
+            second_name varchar(32) not null,
+            username varchar(32) not null,
+            primary key (user_id)
+        );
+
+        create table project_user_relation (
+           project_id uuid not null,
+            user_id uuid not null,
+            primary key (project_id, user_id)
+        );
+
+        create table projects (
+           project_id uuid not null,
+            project_name varchar(64) not null,
+            primary key (project_id)
+        );
+
+
+        create table requests (
+           request_id uuid not null,
+            request_name varchar(32) not null,
+            request_description varchar(255) not null,
+            request_owner uuid,
+            project_id uuid,
+            primary key (request_id)
+        );
+        create table responses (
+           response_id uuid not null,
+            response_comment varchar(255) not null,
+            response_type varchar(255),
+            request_id uuid,
+            response_owner uuid,
+            primary key (response_id)
+        );
+
